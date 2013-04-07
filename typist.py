95fa914b3ce215f0d56233411f360e4288b8e16f
Antoine Balaresque <antoinebalaresque@berkeley.edu>
2013-04-06 17:33:49 -0700

    started process

--- typist.py
+++ typist.py
@@ -1,8 +1,40 @@
 import sys
 
+welcome_msg = "\nWelcome to Typist for Programmers!\n\nType 'start' to start learning practicing on your typing or type 'commands' for a list of commands\n"
+
+
+
 def main():
-	print "test"
+	print welcome_msg
+	inpt = get_input()
+	execute(inpt)
 	return 0
+
+
+def get_input():
+	user_input = raw_input("Type: ")
+	return user_input 
+
+
+def execute(inpt):
+	
+	if inpt ==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
 	exit = main()
